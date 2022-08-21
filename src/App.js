01e3bf8485ad619667215645bc7395b7448d068a
JoseDonaire <josedonairebajo@gmail.com>
2022-08-21 19:37:51 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar'
+import { Routes,Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import AddTravel from './pages/travels/AddTravel';
+import TravelDetails from './pages/travels/TravelDetails';
+import TravelEdit from './pages/travels/TravelEdit';
+import Error from './pages/Error';
+import NotFound from './pages/NotFound'
+import Signup from './pages/auth/Signup';
+import Login from './pages/auth/Login';
+import Profile from './pages/auth/Profile';
+import ProfileDriver from './pages/auth/ProfileDriver';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Navbar/>
+
+
+    <Routes>
+
+    <Route to='/' element={<Home/>} />
+    <Route to='/travels/add' element={<AddTravel/>} />
+    <Route to='/travels/:id/details' element={<TravelDetails/>} />
+    <Route to='/travels/:id/edit' element={<TravelEdit/>} />
+    <Route to='/error' element={<Error/>} />
+    <Route to='/*' element={<NotFound/>} />
+    <Route to='/login' element={<Login/>} />
+    <Route to='/signup' element={<Signup/>} />
+    <Route to='/profile' element={<Profile/>} />
+    <Route to='/profile/driver' element={<ProfileDriver/>} />
+    
+
+    </Routes>
+      
     </div>
   );
 }
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import{useState} from 'react'
 
 function Signup() {
+  const [username,setUserName]=useState('');
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+  const[error,setError]=useState('');
+  const[errorMessage,setErrorMessage]=useState('');
+
+
+  try {
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+
   return (
     <div>Signup</div>
   )
